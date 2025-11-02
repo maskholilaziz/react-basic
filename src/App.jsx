00fdbfd4d2c28c07d7210d63e2976c6fd7ce7372
Muhammad Kholil Aziz Ashari <muhammadkholilazizashari@gmail.com>
2025-11-02 12:34:08 +0700

    Props & Props Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ import Header from './components/Header.jsx'
 const name = 'Muhammad Kholil Aziz Ashari';
 
 function Welcome() {
-  return <h1>Siang boy, {name}!</h1>;
+  return <h1>Welcome {name}</h1>
+}
+
+function UserCardProps(props) {
+  return (
+    <>
+      <h2>Nama : {props.name}</h2>
+      <p>Umur : {props.age} tahun</p>
+    </>
+  );
+}
+
+function UserCardDestructuring({ name, age }) {
+  return (
+    <>
+      <h2>Nama : {name}</h2>
+      <p>Umur : {age} tahun</p>
+    </>
+  );
 }
 
 function App() {
@@ -13,9 +31,9 @@ function App() {
     <>
       <div>
         <Header />
-        <Welcome />
-        <Welcome />
-        <Welcome />
+        <UserCardProps name={name} age={30} />
+        <UserCardDestructuring name="Boyoken" age={-10} />
+        <UserCardDestructuring name="Puegel" age={100} />
         <p>Belajar JSX itu menyenangkan! 🚀</p>
         <button onClick={() => alert("Touched!")}>Touch me</button>
       </div>
